a7f5d4054bbfe20358b3ebc85a6a0ed7ea4f4717
chasmani <pilgrimcharlie2@gmail.com>
2017-04-29 23:53:20 +0100

    Working on binary tree

--- In Progress/binary_tree/binary_search_tree.js
+++ In Progress/binary_tree/binary_search_tree.js
@@ -3,7 +3,8 @@ var context = canvas.getContext('2d');
 
 setup();
 
-var rootNode = new Node(10);
+var rootNode = new Node(10, canvas.width/2,100,1);
+rootNode.draw();
 
 numbers = [1,4,21,8,4,7,2,9,16]
 
@@ -19,26 +20,62 @@ function setup() {
 	canvas.style.backgroundColor = '#262626';
 }
 
-function Node(key) {
+function Node(key, x, y, level) {
 
 	this.key = key;
 	this.left = null;
 	this.right = null;
+	this.x = x;
+	this.y = y;
+	this.level = level;
+
+	this.nodeGap = canvas.width/(Math.pow(2, this.level+1))
+
+	this.radius = 0;
 
 	this.insert = function(value) {
 		if (value<this.key) {
 			if (this.left == null){
-				this.left = new Node(value);
+				this.left = new Node(value, this.x - this.nodeGap, this.y+150, this.level+1);
+				this.left.draw();
 			} else {
 				this.left.insert(value);
 			}
 		}
 		else if (value>this.key) {
 			if (this.right == null){
-				this.right = new Node(value);
+				this.right = new Node(value, this.x + this.nodeGap, this.y+150, this.level+1);
+				this.right.draw();
 			} else {
 				this.right.insert(value);
 			}
+		}
+	}
+
+	this.draw = function() {
+		
+		if(this.radius<30){
+			this.radius += 1;
+			}
+
+		context.beginPath();
+		context.arc(this.x,this.y,this.radius,0,2*Math.PI);
+		context.stroke();	
+
+
+		if(this.radius==30){		
+			context.textAlign="center";
+			context.textBaseline="middle"; 
+			context.font="30px Arial";
+			context.fillText(this.key,this.x,this.y);	
+			
+			if(this.left){
+				this.left.draw();
+			}
+			if(this.right){
+				this.right.draw();
+			}
+			
 		}
 	}
 }
@@ -65,6 +102,14 @@ function printOrdered(node) {
 	}
 }
 
+
+
+function drawTree(node) {
+	context.clearRect(0,0,canvas.width,canvas.height);
+	rootNode.draw();
+}
+
+
 // Print tree to console
 console.log(rootNode);
 
@@ -75,6 +120,7 @@ console.log(searchRecursively(7, rootNode));
 printOrdered(rootNode);
 
 function draw() {
+	drawTree();
 	requestAnimationFrame(draw);
 }
 
