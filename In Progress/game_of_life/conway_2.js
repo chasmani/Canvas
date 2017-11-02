7d3637b893c342cb677e7bab6f53d740eed9a867
chasmani <pilgrimcharlie2@gmail.com>
2017-11-02 14:59:41 +0000

    Slider input working in basic way

--- In Progress/game_of_life/conway_2.js
+++ In Progress/game_of_life/conway_2.js
@@ -2,7 +2,7 @@
 var canvas = document.getElementById('conway');
 var context = canvas.getContext('2d');
 
-var cellCountX = 32;
+var cellCountX = 12;
 var cellCountY = 32;
 
 var cellWidth = 0;
@@ -10,8 +10,31 @@ var cellHeight = 0;
 
 var borderColor = "#333";
 
+var cells = []
+var currentTime = Date.now();
+var nextCells = []
+var timeSinceLastFrame = 10000;
+var neighbourLocations = []
 
-var neighbourLocations = [
+
+function setup() {	
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	canvas.style.backgroundColor = '#ddd';
+	cellWidth = canvas.width/cellCountX;
+	
+	cellHeight = canvas.height/cellCountY;
+
+	cells = []
+
+	for (i=0;i<cellCountX;i++){
+		for(j=0;j<cellCountY;j++){
+			var state = Math.round(Math.random());
+			cells.push(state);
+		}
+	}
+
+	neighbourLocations = [
 	-1-cellCountX,
 	-cellCountX,
 	1-cellCountX,
@@ -20,47 +43,29 @@ var neighbourLocations = [
 	-1+cellCountX,
 	cellCountX,
 	+1+cellCountX
-]
+	]
 
-function setup() {	
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	canvas.style.backgroundColor = '#ddd';
-	cellWidth = canvas.width/cellCountX;
-	cellHeight = canvas.height/cellCountY;
+	nextCells = []
+
+
 }
 
-var cells = []
-var currentTime = Date.now();
-var nextCells = []
-var timeSinceLastFrame = 10000;
 
 
-for (i=0;i<cellCountX;i++){
-	for(j=0;j<cellCountY;j++){
-		var state = Math.round(Math.random());
-		cells.push(state);
-	}
-}
 
 setup();
+
 
 
 function draw(){
 
 	timeSinceLastFrame = Date.now() - currentTime;
 
-	if (timeSinceLastFrame > 90){
+	if (timeSinceLastFrame > 100){
 		currentTime = Date.now();
-
-
-
-		console.log(cells.length);
-
-
+		
 		for (k=0;k<cells.length;k++){
 			
-
 			// Count the neighbours alive
 			var neighboursAlive = 0;
 			for (l=0;l<neighbourLocations.length;l++){
@@ -69,9 +74,9 @@ function draw(){
 					neighbourPos -= cells.length
 				}
 				neighboursAlive += cells[neighbourPos];
-
-				
 			}
+
+			
 
 			// Populate nextCells for next frame
 			if (neighboursAlive==3) {
@@ -83,10 +88,7 @@ function draw(){
 			else{
 				nextCells.push(0)
 			}
-			
-
-
-
+		
 
 			// Draw this frame
 			if (cells[k]){
@@ -106,6 +108,8 @@ function draw(){
 			
 		}
 
+		
+
 		cells = nextCells;
 		nextCells = [];
 
@@ -115,6 +119,24 @@ function draw(){
 
 	requestAnimationFrame(draw)
 }
+
+
+
+cellsHorizontalInput = document.getElementById("cellsHorizontal");
+cellsHorizontalValue = document.getElementById("cellsHorizontal-value");
+
+cellsHorizontalInput.addEventListener("change", function() {
+    
+	
+	cellsHorizontalValue.textContent = cellsHorizontalInput.value;
+
+	cellCountX = parseInt(cellsHorizontalInput.value);
+	setup();
+
+
+    
+}, false);
+
 
 
 draw();
