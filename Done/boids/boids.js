892cc505d461f78b06f99245d78c6890acd5a09e
chasmani <pilgrimcharlie2@gmail.com>
2017-11-29 14:24:32 +0000

    Boids done

--- Done/boids/boids.js
+++ Done/boids/boids.js
@@ -6,43 +6,44 @@ canvas.height = canvas.offsetHeight;
 canvas.width = canvas.offsetWidth;
 
 /* Colour scheme */
-canvas.style.backgroundColor = "#44BBFF";
+canvas.style.backgroundColor = "#6BB9F0";
+var boidColor = "#27AE60";
+var boidOutline = "#ECF0F1";
+var visionCircleColor = "#C5EFF7";
+var alignColor = "#C5EFF7";
+var sepColor = "yellow";
+var cohColor = "pink";
 
 /* Parameters */
-var boidColor = "#112233";
-
 var boidVisionLength = 50; 
-var visionCircleColor = "#fff";
-
 var alignmentWeight = 2;
 var cohesionWeight = 1;
 var seperationWeight = 2;
 var maxSepForce  =20;
-
 var maxVelocity = 2;
 var maxForce = 0.05;
 
-var showForces = true;
+var showForces = false;
 
+// Create boids
+var boidCount = Math.floor(canvas.width/10);
 var boids = createBoids()
 
-// Create the initial boids
 function createBoids() {
 	newBoids = []
-	for (i=0; i<100; i++){
+
+	for (i=0; i<boidCount; i++){
+		console.log("hello");
 		newBoid = new Boid();
 		newBoids.push(newBoid);
 	}
 	return newBoids
 }
 
+
 function Vector(x, y) {
   this.x = x;
   this.y = y;
-};
-
-Vector.prototype.toArray = function() {
-  return [this.x, this.y];
 };
 
 // return the angle of the vector in radians
@@ -55,8 +56,6 @@ Vector.prototype.getMagnitude = function() {
 	// use pythagoras theorem to work out the magnitude of the vector
 	return Math.sqrt(this.x * this.x + this.y * this.y);
 };
-
-
 
 // add a vector to this one
 Vector.prototype.addTo = function(v2, weight) {
@@ -91,78 +90,41 @@ Vector.prototype.max = function(maxMagnitude) {
 	}
 }
 
-
-// get opposite vector
-Vector.prototype.invert = function() {
-	
-	if (this.x!=0){
-		this.x = -1/this.x;	
-	}
-	if (this.y!=0){
-		this.y = -1/this.y;	
-	}
-	
-	
-};
-
 // Get distance from this to another vector
 Vector.prototype.getDistance = function(v2) {
 	return Math.sqrt(Math.pow((this.x-v2.x), 2) + Math.pow((this.y-v2.y), 2))
 };
 
+// Get te inverse square force of two position vectors
 Vector.prototype.inverseSquareForce = function(v2){
-
 	dx = this.x-v2.x;
 	dy = this.y-v2.y;
-
 	distanceSquared = Math.pow((dx), 2) + Math.pow((dy), 2)
 	force = Math.min(1/distanceSquared, 1);
 	return new Vector(dx*force, dy*force);
-
-
 }
 
 
+function Boid(x,y) {
 
-// 1. Alignement
-// Add alignment of all nearby boids
-// Divide by neighbour count (get vaerage velocity)
-// Scaling factor i.e. normalise
-// https://gamedevelopment.tutsplus.com/tutorials/3-simple-rules-of-flocking-behaviors-alignment-cohesion-and-separation--gamedev-3444
-// EASIER IN CARTESIAN
-
-// 2. Cohesion
-// Add position of all nearbby boids 
-// Divide by neighbour count (get average position)
-// Put it in relation to this boid (x-x0) etc
-// Normalise it to 1
-// EASIER IN CARTESIAN
-
-// Seperation
-
-
-
-function Boid() {
-
-	this.position = new Vector(canvas.width/2, canvas.height/2)
+	if((x)&&(y)){
+		this.position = new Vector(x, y)	
+	} else {
+		this.position = new Vector(canvas.width/2, canvas.height/2)	
+	}
 	
 	this.velocity = new Vector(maxVelocity*(0.5-Math.random()), maxVelocity*(0.5-Math.random()))
 	
-
 	this.localBoids = []
 
-	this.move = function() {
-		
+	this.move = function() {	
 		this.calculateVelocity();
-		
 		this.position.addTo(this.velocity);
-		
 		this.wrapSides();
-	
 	}
 
+	// Wrap boids as they go off the screen
 	this.wrapSides = function() {
-		
 		if (this.position.x<-50){
 			this.position.x += (canvas.width+50);
 		}
@@ -178,25 +140,21 @@ function Boid() {
 		}
 	}
 
-	this.speedLimit = function() {
-		// add as function in vector
-	}
-
-
-
+	// Draw the boid itself
 	this.draw = function() {
 		
 		context.save();
-		
 		context.translate(this.position.x, this.position.y);
-		
 		context.rotate(this.velocity.getDirection());
+		context.strokeStyle = boidOutline;
+		context.fillStyle = boidColor;
 		context.beginPath();
 		context.moveTo(0, 0);
 		context.lineTo(-20, 5);
 		context.lineTo(-20, -5);
 		context.closePath();
 		context.fill();
+		context.stroke();
 		context.restore();
 	}
 
@@ -215,9 +173,8 @@ function Boid() {
 	}
 
 	// Set an array of boids within vision range (array of index numbers)
-	// To do - add wrapping (maybe)
+	// It doesn't consider boids wrapped on the other side of the screen 
 	this.findLocalBoids = function() {
-
 		this.localBoids = [];
 		for (var k=0; k<boids.length; k++){
 			var distance = this.position.getDistance(boids[k].position);
@@ -225,7 +182,6 @@ function Boid() {
 				this.localBoids.push(k)
 			}
 		}
-
 		if (showForces){
 			context.beginPath();
 			context.arc(this.position.x,this.position.y,boidVisionLength,0,2*Math.PI);
@@ -235,11 +191,12 @@ function Boid() {
 	}
 
 	// Get the alignment force vector
+	// Equal to the sum of all neighbouring boid's alignment vectors
 	this.alignmentVector = function() {
 		
 		alignV = new Vector(0,0);
 		
-		// Sum alginments of all local boids
+		// Sum alignments of all local boids
 		for (var m=0; m<this.localBoids.length; m++){
 			alignV.addTo(boids[this.localBoids[m]].velocity)
 		}
@@ -252,14 +209,14 @@ function Boid() {
 			context.beginPath();	
 			context.moveTo(this.position.x,this.position.y);
 			context.lineTo(this.position.x+((boidVisionLength/maxForce)*alignV.x),this.position.y+((boidVisionLength/maxForce)*alignV.y));
-			context.strokeStyle = "yellow";
+			context.strokeStyle = alignColor;
 			context.stroke();		
 		}
 
 		return alignV;	
 	}
 
-	// Move towards center of mass
+	// Cohesion force vector points towards center of mass of neighbouring boids
 	this.cohesionVector = function() {
 
 		cohesionV = new Vector(0,0);
@@ -269,13 +226,13 @@ function Boid() {
 			cohesionV.addTo(boids[this.localBoids[l]].position)
 		}
 
-		// Get average position
+		// Get average position - necessary because these are absolute poisiotns at this point
 		cohesionV.average(this.localBoids.length)
 
-		// Get relative position
+		// Covert to a relative position
 		cohesionV.subtractFrom(this.position);
 		
-		// Normalise alignment vector to magnitude 1
+		// Limit the magnitude of the force
 		cohesionV.max(maxForce);
 		
 		if (showForces){
@@ -283,53 +240,47 @@ function Boid() {
 			context.beginPath();	
 			context.moveTo(this.position.x,this.position.y);
 			context.lineTo(this.position.x+((boidVisionLength/maxForce)*cohesionV.x),this.position.y+((boidVisionLength/maxForce)*cohesionV.y));
-			context.strokeStyle = "green";
+			context.strokeStyle = cohColor;
 			context.stroke();
 			}		
 
 		return cohesionV;	
-
 	}
 
-
-	// Need to replace this iwth an inverse square type law
+	// Seperation force is repulsive and follows an inverse square law 
 	this.seperationVector = function() {
 
 		sepV = new Vector(0,0);
 
-		// Sum distance from current boid of all local neighbours
+		// Sum repulsive forces of all neighbouring boids
 		for (var n=0; n<this.localBoids.length; n++){
-
 			sepV.addTo(this.position.inverseSquareForce(boids[this.localBoids[n]].position))
 		}
 		
+		// Limit size of seperation force
 		sepV.max(maxForce);
 
 		if (showForces){
 			// Draw seperation vector
 			context.beginPath();	
 			context.moveTo(this.position.x,this.position.y);
-			//context.lineTo(this.position.x+((boidVisionLength/maxForce)*cohesionV.x),this.position.y+((boidVisionLength/maxForce)*cohesionV.y));
-			context.strokeStyle = "red";
+			context.strokeStyle = sepColor;
 			context.stroke();		
 		}
 
 		return sepV;		
-
 	}
-
-
-
-
 }
 
-
+// Main draw function called once per frame
 function draw() {
 	context.clearRect(0,0,canvas.width,canvas.height);
+	// Move the boids following rules and then draw
 	for (var j=0; j<boids.length; j++){
 		boids[j].move();
 		boids[j].draw();
 	}
+	// Keep looping
   	requestAnimationFrame(draw);
 }
 
@@ -340,7 +291,6 @@ function resizeCanvas() {
 	canvas.height = canvas.offsetHeight;
 	canvas.width = canvas.offsetWidth;
 }
-
 
 var canvasSection = document.getElementById("article-canvas");
 
@@ -357,10 +307,9 @@ document.getElementById("leave-fullscreen-button").addEventListener("click", fun
 	document.body.style.overflow="scroll";
 });
 
-
 window.addEventListener("resize", resizeCanvas);
 
-
+// Add a button to toggle showing forces on the boids
 var toggleShowForcesButton = document.createElement("button");
 toggleShowForcesButton.innerHTML = "Toggle Show Forces";
 toggleShowForcesButton.className += "btn btn-default"
@@ -375,3 +324,16 @@ toggleShowForcesButton.addEventListener ("click", function() {
 		showForces = true;
 	}
 });
+
+// Add new boids if the suer clicks the screen
+document.addEventListener("click", function(){
+	var canvasRect = canvas.getBoundingClientRect();    
+	cursor_x = event.clientX - canvasRect.left;
+	cursor_y = event.clientY - canvasRect.top;
+	if ((cursor_x>0)&&(cursor_x<canvas.width)&&(cursor_y>0)&&(cursor_y<canvas.height)){
+		newBoid = new Boid(cursor_x, cursor_y);
+		boids.push(newBoid);
+	}
+});
+
+	
