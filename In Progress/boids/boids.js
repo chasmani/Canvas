264ed9963f87f2f603cd5d6b7b35a9448686177a
chasmani <pilgrimcharlie2@gmail.com>
2017-11-29 13:24:07 +0000

    Boids is working

--- In Progress/boids/boids.js
+++ In Progress/boids/boids.js
@@ -11,21 +11,25 @@ canvas.style.backgroundColor = "#44BBFF";
 /* Parameters */
 var boidColor = "#112233";
 
-var boidVisionLength = 100; 
+var boidVisionLength = 50; 
 var visionCircleColor = "#fff";
 
-/* Alignment */
-var alignmentWeight = 0.02;
-var cohesionWeight = 0.01;
-var seperationWeight = 0.8;
+var alignmentWeight = 2;
+var cohesionWeight = 1;
+var seperationWeight = 2;
+var maxSepForce  =20;
 
+var maxVelocity = 2;
+var maxForce = 0.05;
+
+var showForces = true;
 
 var boids = createBoids()
 
 // Create the initial boids
 function createBoids() {
 	newBoids = []
-	for (i=0; i<20; i++){
+	for (i=0; i<100; i++){
 		newBoid = new Boid();
 		newBoids.push(newBoid);
 	}
@@ -77,14 +81,16 @@ Vector.prototype.average = function(quantity) {
   	this.y /= quantity;
 };
 
-// Normalise to desired magnitude
-Vector.prototype.normalise = function(desiredMagnitude) {
-	if (this.getMagnitude() !=0){
-		normisationFactor = this.getMagnitude()/desiredMagnitude;
-		this.x /= normisationFactor;
-	  	this.y /= normisationFactor;
-	  }
-};
+
+// Set maximum value
+Vector.prototype.max = function(maxMagnitude) {
+	currentMagnitude = this.getMagnitude(); 
+	if (currentMagnitude > maxMagnitude) {
+		this.x /= (currentMagnitude/maxMagnitude);
+		this.y /= (currentMagnitude/maxMagnitude);
+	}
+}
+
 
 // get opposite vector
 Vector.prototype.invert = function() {
@@ -104,7 +110,17 @@ Vector.prototype.getDistance = function(v2) {
 	return Math.sqrt(Math.pow((this.x-v2.x), 2) + Math.pow((this.y-v2.y), 2))
 };
 
+Vector.prototype.inverseSquareForce = function(v2){
 
+	dx = this.x-v2.x;
+	dy = this.y-v2.y;
+
+	distanceSquared = Math.pow((dx), 2) + Math.pow((dy), 2)
+	force = Math.min(1/distanceSquared, 1);
+	return new Vector(dx*force, dy*force);
+
+
+}
 
 
 
@@ -130,15 +146,14 @@ function Boid() {
 
 	this.position = new Vector(canvas.width/2, canvas.height/2)
 	
-	this.velocity = new Vector(0.5-Math.random(), 0.5-Math.random())
+	this.velocity = new Vector(maxVelocity*(0.5-Math.random()), maxVelocity*(0.5-Math.random()))
 	
 
 	this.localBoids = []
 
 	this.move = function() {
 		
-		this.reynolds();
-		
+		this.calculateVelocity();
 		
 		this.position.addTo(this.velocity);
 		
@@ -178,15 +193,15 @@ function Boid() {
 		context.rotate(this.velocity.getDirection());
 		context.beginPath();
 		context.moveTo(0, 0);
-		context.lineTo(-50, 10);
-		context.lineTo(-50, -10);
+		context.lineTo(-20, 5);
+		context.lineTo(-20, -5);
 		context.closePath();
 		context.fill();
 		context.restore();
 	}
 
 	// Combine forces acting on boid, apply force to velocity as an acceleration
-	this.reynolds = function() {
+	this.calculateVelocity = function() {
 		
 		this.findLocalBoids();
 		var alignment = this.alignmentVector();
@@ -196,8 +211,7 @@ function Boid() {
 		this.velocity.addTo(alignment, alignmentWeight);
 		this.velocity.addTo(cohesion, cohesionWeight);
 		this.velocity.addTo(seperation, seperationWeight);
-		this.velocity.normalise(1);
-
+		this.velocity.max(maxVelocity);
 	}
 
 	// Set an array of boids within vision range (array of index numbers)
@@ -212,11 +226,12 @@ function Boid() {
 			}
 		}
 
-		context.beginPath();
-		
-		context.arc(this.position.x,this.position.y,boidVisionLength,0,2*Math.PI);
-		context.strokeStyle= visionCircleColor;
-		context.stroke();
+		if (showForces){
+			context.beginPath();
+			context.arc(this.position.x,this.position.y,boidVisionLength,0,2*Math.PI);
+			context.strokeStyle= visionCircleColor;
+			context.stroke();
+		}
 	}
 
 	// Get the alignment force vector
@@ -229,18 +244,17 @@ function Boid() {
 			alignV.addTo(boids[this.localBoids[m]].velocity)
 		}
 
-		// Average alignment
-		alignV.average(this.localBoids.length);
-		
-		// Normalise alignment vector to magnitude 1
-		alignV.normalise(1);
+		// Limit alignment force
+		alignV.max(maxForce);
 
-		// Draw alignment vector
-		context.beginPath();	
-		context.moveTo(this.position.x,this.position.y);
-		context.lineTo(this.position.x+(boidVisionLength*alignV.x),this.position.y+(boidVisionLength*alignV.y));
-		context.strokeStyle = "yellow";
-		context.stroke();		
+		if (showForces){
+			// Draw alignment vector
+			context.beginPath();	
+			context.moveTo(this.position.x,this.position.y);
+			context.lineTo(this.position.x+((boidVisionLength/maxForce)*alignV.x),this.position.y+((boidVisionLength/maxForce)*alignV.y));
+			context.strokeStyle = "yellow";
+			context.stroke();		
+		}
 
 		return alignV;	
 	}
@@ -255,21 +269,23 @@ function Boid() {
 			cohesionV.addTo(boids[this.localBoids[l]].position)
 		}
 
-		// Average positions
-		cohesionV.average(this.localBoids.length);
-				
+		// Get average position
+		cohesionV.average(this.localBoids.length)
+
 		// Get relative position
 		cohesionV.subtractFrom(this.position);
 		
 		// Normalise alignment vector to magnitude 1
-		//cohesionV.normalise(1);
+		cohesionV.max(maxForce);
 		
-		// Draw alignment vector
-		context.beginPath();	
-		context.moveTo(this.position.x,this.position.y);
-		context.lineTo(this.position.x+(boidVisionLength*cohesionV.x),this.position.y+(boidVisionLength*cohesionV.y));
-		context.strokeStyle = "green";
-		context.stroke();		
+		if (showForces){
+			// Draw alignment vector
+			context.beginPath();	
+			context.moveTo(this.position.x,this.position.y);
+			context.lineTo(this.position.x+((boidVisionLength/maxForce)*cohesionV.x),this.position.y+((boidVisionLength/maxForce)*cohesionV.y));
+			context.strokeStyle = "green";
+			context.stroke();
+			}		
 
 		return cohesionV;	
 
@@ -284,24 +300,19 @@ function Boid() {
 		// Sum distance from current boid of all local neighbours
 		for (var n=0; n<this.localBoids.length; n++){
 
-			sepV.addTo(boids[this.localBoids[n]].position);
-			sepV.subtractFrom(this.position);
+			sepV.addTo(this.position.inverseSquareForce(boids[this.localBoids[n]].position))
 		}
-
-		// Average positions
-		sepV.average(this.localBoids.length);
 		
-		// Normalise with negative 1 to get repulsive force
-		sepV.invert()
+		sepV.max(maxForce);
 
-		sepV.normalise(1);
-
-		// Draw seperation vector
-		context.beginPath();	
-		context.moveTo(this.position.x,this.position.y);
-		context.lineTo(this.position.x+(boidVisionLength*cohesionV.x),this.position.y+(boidVisionLength*cohesionV.y));
-		context.strokeStyle = "red";
-		context.stroke();		
+		if (showForces){
+			// Draw seperation vector
+			context.beginPath();	
+			context.moveTo(this.position.x,this.position.y);
+			//context.lineTo(this.position.x+((boidVisionLength/maxForce)*cohesionV.x),this.position.y+((boidVisionLength/maxForce)*cohesionV.y));
+			context.strokeStyle = "red";
+			context.stroke();		
+		}
 
 		return sepV;		
 
@@ -349,3 +360,18 @@ document.getElementById("leave-fullscreen-button").addEventListener("click", fun
 
 window.addEventListener("resize", resizeCanvas);
 
+
+var toggleShowForcesButton = document.createElement("button");
+toggleShowForcesButton.innerHTML = "Toggle Show Forces";
+toggleShowForcesButton.className += "btn btn-default"
+
+var buttonContainer = document.getElementById("canvas-buttons");
+buttonContainer.appendChild(toggleShowForcesButton);
+
+toggleShowForcesButton.addEventListener ("click", function() {
+	if(showForces){
+		showForces = false;
+	} else {
+		showForces = true;
+	}
+});
